bafaa3b7b149890bfb2e7f1d7b19ba61b4ffca63
Ritesh Rao <ritesh_rao@thbs.india.com>
2024-02-19 16:52:23 +0530

    fixed forgot password feature

--- src/main/java/com/dietify/v1/Service/EmailService.java
+++ src/main/java/com/dietify/v1/Service/EmailService.java
@@ -2,57 +2,31 @@ package com.dietify.v1.Service;
 
 import java.util.Properties;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class EmailService {
 
-    private final String username;
-    private final String password;
-    private final Properties props;
+    @Autowired
+    private JavaMailSender mailSender;
+    public void sendEmail(String toEmail,
+                                String subject,
+                                String body
+    ) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("fromemail@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+        mailSender.send(message);
+        System.out.println("Mail Send...");
 
-    public EmailService() {
-        this.username = "ritheshvr0@gmail.com"; // Your email address
-        this.password = "Ritesh@@016"; // Your email password
 
-        // Initialize SMTP properties
-        props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com"); // Replace with your SMTP server
-        props.put("mail.smtp.port", "587"); // Replace with your SMTP port
-    }
-
-    public void sendEmail(String to, String subject, String body) {
-        Session session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
-
-        try {
-            // Create a new message
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(username));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-            message.setSubject(subject);
-            message.setText(body);
-
-            // Send the message
-            Transport.send(message);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-            // Handle exception or log error
-        }
     }
 }
+
